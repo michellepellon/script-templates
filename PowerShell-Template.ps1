42ddcd6b3d99c4ad9a1cff4bec818127f16ef4e1
Michelle Pellon <122621769+michellepellon@users.noreply.github.com>
2023-03-13 08:35:04 -0500

    Initial commit.

---
+++ PowerShell-Template.ps1
@@ -0,0 +1,77 @@
+#requires -version 2
+<#
+.SYNOPSIS
+    <Overview of the script>
+
+.DESCRIPTION
+    <Brief description of the script>
+
+.PARAMETER <Parameter_Name>
+    <Brief description of the parameter input required. Repeat this attribute if required.>
+
+.INPUTS
+    <Inputs if any, otherwise state None>
+
+.OUTPUTS
+    <Outputs if any, otherwise state None>
+
+.NOTES
+    Version:            0.1.0
+    Author:             <Name>
+    Creation Date:      <Date>
+    Purpose/Change:     Initial release
+
+.EXAMPLE
+    <Example goes here. Repeat this attribute for more than one example>
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = "SilentlyContinue"
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Script Version
+$sScriptVersion = "1.0"
+
+#Log File Info
+$sLogPath = "C:\Windows\Temp"
+$sLogName = "<script_name>.log"
+$sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+<#
+Function <FunctionName>{
+  Param()
+  
+  Begin{
+    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
+  }
+  
+  Process{
+    Try{
+      <code goes here>
+    }
+    
+    Catch{
+      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
+      Break
+    }
+  }
+  
+  End{
+    If($?){
+      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
+      Log-Write -LogPath $sLogFile -LineValue " "
+    }
+  }
+}
+#>
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+#Log-Start -LogPath $sLogPath -LogName $sLogName -ScriptVersion $sScriptVersion
+#Script Execution goes here
+#Log-Finish -LogPath $sLogFile
---
+++ README.md
@@ -0,0 +1,3 @@
+# Script Templates
+
+Various templates for common scripting languages -- e.g., PowerShell, Bash, etc.
